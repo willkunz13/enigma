440b86802e3e7f3a1108ae91874f798143235589
jerry <jerry@jerrys-MacBook-Pro.local>
2020-01-14 08:43:35 -0700

    Refactor to exclude methods moved to other classes

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -14,84 +14,31 @@ class EnigmaTest < Minitest::Test
 		@key = "02715"
 		@date2 = "123456"
 		@key2 = "12345"
-		@message = "hello, world"
-		@encoded_message = @enigma.encrypt_message(@message, @key, @date)
-	end
-
-	def test_its_variables
-		assert_includes @enigma.whitelist, "a"
-		assert_includes @enigma.whitelist, " "
-		refute_includes @enigma.whitelist, ","
+		@input = "hello, world"
+                @encrypted_message = "keder,sprrdx"
 	end
 
 	def test_it_exists
 		assert_instance_of Enigma, @enigma
 	end
 
-	def test_first
-		assert_equal @enigma.first(@key, @date), 3
-		assert_equal @enigma.first(@key2, @date2), 15		
-	end
-
-	def test_second
-		assert_equal @enigma.second(@key, @date), 27
-		assert_equal @enigma.second(@key2, @date2), 32		
-	end
-
-	def test_third
-		assert_equal @enigma.third(@key, @date), 73
-		assert_equal @enigma.third(@key2, @date2), 37		
-	end
-
-	def test_fourth
-		assert_equal @enigma.fourth(@key, @date), 20
-		assert_equal @enigma.fourth(@key2, @date2), 51		
-	end
-
-	def test_date_extractor
-		assert_equal 1, @enigma.date_extractor(@date, -4)
-		assert_equal 0, @enigma.date_extractor(@date, -3)
-		assert_equal 3, @enigma.date_extractor(@date2, -4)
-		assert_equal 6, @enigma.date_extractor(@date2, -1)	
-	end
-
-	def test_encrypt_iteration
-		assert_equal ["k", "e", "d", "e"], @enigma.encrypt_iteration(@message, [3, 27, 73, 20])[0]
-		assert_equal 3, @enigma.encrypt_iteration(@message, [3, 27, 73, 20]).size
-                
-	end
-
-	def test_encrypt_message
-		assert_equal "kede", @enigma.encrypt_message(@message, @key, @date)[0,4]
-		assert_equal @message.size, @enigma.encrypt_message(@message, @key, @date).size
-	end
-
-	def test_encrypt
-		assert_instance_of Hash, @enigma.encrypt(@message, @key, @date)
-		assert_equal @key, @enigma.encrypt(@message, @key, @date)[:key]
-		assert_equal @date, @enigma.encrypt(@message, @key, @date)[:date]
-		assert_equal @enigma.encrypt_message(@message, @key, @date), @enigma.encrypt(@message, @key, @date)[:encryption]
-	end
-
-	def test_decrypt_iteration
-		assert_equal ["h", "e", "l", "l"], @enigma.decrypt_iteration(@encoded_message, [-3, -27, -73, -20])[0]
-		assert_equal 3, @enigma.decrypt_iteration(@encoded_message, [-3, -27, -73, -20]).size
-	end
-
-	def test_decrypt_message
-		assert_equal "hell", @enigma.decrypt_message(@encoded_message, @key, @date)[0,4]
-		assert_equal @message.size, @enigma.decrypt_message(@encoded_message, @key, @date).size
-	end
+        def test_encrypt
+                assert_instance_of Hash, @enigma.encrypt(@input, @key, @date)
+                assert_equal @key, @enigma.encrypt(@input, @key, @date)[:key]
+                assert_equal @date, @enigma.encrypt(@input, @key, @date)[:date]
+                assert_includes @enigma.encrypt(@input, @key, @date)[:encryption], "kede"
+        end
 
         def test_decrypt
-                assert_instance_of Hash, @enigma.decrypt(@encoded_message, @key, @date)
-		assert_equal @key, @enigma.decrypt(@encoded_message, @key, @date)[:key]
-		assert_equal @date, @enigma.decrypt(@encoded_message, @key, @date)[:date]
-		assert_equal @enigma.decrypt_message(@encoded_message, @key, @date), @enigma.decrypt(@encoded_message, @key, @date)[:decryption]
+                assert_instance_of Hash, @enigma.decrypt(@encrypted_message, @key, @date)
+                assert_equal @key, @enigma.decrypt(@encrypted_message, @key, @date)[:key]
+                assert_equal @date, @enigma.decrypt(@encrypted_message, @key, @date)[:date]
+                assert_includes @enigma.decrypt(@encrypted_message, @key, @date)[:decryption], "hell"
         end
 
         def test_random_key
                 assert_instance_of String, @enigma.random_key
                 assert_equal 5, @enigma.random_key.size
         end
+
 end
